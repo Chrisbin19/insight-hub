8af98ea3431f73635b64c042b6b00e6f16dd1012
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-08-26 23:15:00 +0530

    Some more modifications on the express server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ dotenv.config();
 import express from 'express';
 const app = express();
 
+app.get('/getRestaurants',(req,res)=>{
+   res.json({
+    "status":"success",
+    "restaurant":"mcdonalds"
+   });
+});    
+
 const port= process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`Server is up and listening on port ${port}`);
