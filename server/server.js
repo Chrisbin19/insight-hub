54cfa74001de74390108466341fef73a59f4749d
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-08-26 23:51:27 +0530

    configure the PUT route

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ app.get('/api/v1/restaurants/:id',(req,res)=>{
     console.log(req);
 })
 
+app.post('/api/v1/restaurants',(req,res)=>{
+    console.log(req);
+})
 const port= process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`Server is up and listening on port ${port}`);
