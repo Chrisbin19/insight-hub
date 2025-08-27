ffd2b8216d93ec146fc4847bea9bc594e158c127
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-08-27 20:48:22 +0530

    Home page layout modified

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -2,30 +2,37 @@ import React from 'react'
 
 const AddRestaurant = () => {
   return (
-    <div className='mb-4'>
-        <form action="">
-            <div className="row">
-                <div className="col-md">
-                    <input type="text" className='form-control' placeholder='name'/>
-                </div>
-                <div className="col-md">
-                    <input type="text" className='form-control' placeholder='location'/>
-                </div>
-                <div className='col-md'>
-                    <select className='custom-select my-1 mr-sm-2'>
-                        <option>Price Range</option>
-                        <option value="1">$</option>
-                        <option value="2">$$</option>
-                        <option value="3">$$$</option>
-                        <option value="4">$$$$</option>
-                        <option value="5">$$$$$</option>
-                    </select>
-                </div>
-                <div className="col-md">
-                    <button className="btn btn-primary">Add</button>
-                </div>
-            </div>
-        </form>
+    <div className="mb-4">
+      <form className="d-flex justify-content-center">
+        {/* Name input */}
+        <input 
+          type="text" 
+          className="form-control mx-2 w-25" 
+          placeholder="name" 
+        />
+
+        {/* Location input */}
+        <input 
+          type="text" 
+          className="form-control mx-2 w-25" 
+          placeholder="location" 
+        />
+
+        {/* Price range dropdown */}
+        <select className="form-control mx-2 w-25">
+          <option disabled selected>Price Range</option>
+          <option value="1">$</option>
+          <option value="2">$$</option>
+          <option value="3">$$$</option>
+          <option value="4">$$$$</option>
+          <option value="5">$$$$$</option>
+        </select>
+
+        {/* Add button */}
+        <button className="btn btn-primary mx-2 px-4">
+          Add
+        </button>
+      </form>
     </div>
   )
 }
