b34e67a85cdacb65f5493d1b2a2fdca178a99947
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-08-27 17:07:26 +0530

    Completed the routing of React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import RestaurantdetailPage from './routes/RestaurantdetailPage';
+import Home from './routes/Home';
+import UpdatePage  from './routes/UpdatePage';
 const App = ()=>{
-    return (<div>App</div>)
+    return (<div>
+        <Router>
+            <Routes>
+             <Route path="/" element={<Home />} />
+             <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+             <Route path="/restaurants/:id" element={<RestaurantdetailPage />} />
+            </Routes>
+        </Router>
+    </div>)
 };
 
 export default App;
