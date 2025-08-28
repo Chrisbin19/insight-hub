64c77cde94222846cca34e9d44aa1812f8ae3858
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-08-28 21:10:15 +0530

    Axios implementation and connecting to DB

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
 import db from './db/index.js';
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 
