32ef62ecd8e8b51899fe40016f1cc51a540cb96d
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-09-09 02:27:41 +0530

    Implementeed the initail configuration of star rating

--- client/src/routes/RestaurantdetailPage.jsx
+++ client/src/routes/RestaurantdetailPage.jsx
@@ -2,6 +2,7 @@ import React, { useContext,useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { RestaurantsContext } from '../context/RestaurantsContext';
 import RestaurantFinder from '../apis/RestaurantFinder';
+import StarRating from '../components/StarRating';
 
 const RestaurantdetailPage = () => {
   const {id} = useParams();
@@ -21,7 +22,7 @@ const RestaurantdetailPage = () => {
     },[]
   );
   return (
-    <div>{selectedRestaurants && selectedRestaurants.name}</div>
+    <div>{selectedRestaurants && <StarRating rating= {3.5}/>}</div>
   )
 }
 
