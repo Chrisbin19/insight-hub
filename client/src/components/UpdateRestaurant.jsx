5ce93a002e0ffaf48877e74a14e78e1544811b19
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-09-08 23:25:43 +0530

    Update page has been created which will be triggered upon clicking update buton

---
+++ client/src/components/UpdateRestaurant.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+import {useParams} from 'react-router-dom';
+const UpdateRestaurant = (props) => {
+  const {id} = useParams();
+  const [name,setName] = useState("");
+  const [location,setLocation] = useState("");
+  const [priceRange,setPriceRange] = useState("");
+  
+  return (
+    <div>
+        <form action="">
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input value ={name} onChange={(e)=>{setName(e.target.value)}} id = 'name' class = 'form-control' type="text" />
+            </div>
+             <div className="form-group">
+                <label htmlFor="location">Location</label>
+                <input value ={location} onChange={(e)=>{setLocation(e.target.value)}} id = 'location' class = 'form-control' type="text" />
+            </div>
+             <div className="form-group">
+                <label htmlFor="price_range">Price Range</label>
+                <input value ={priceRange} onChange={(e)=>{setPriceRange(e.target.value)}}  id = 'price_range' class = 'form-control' type='number' />
+            </div>
+            <button className='btn btn-primary'>Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default UpdateRestaurant 
