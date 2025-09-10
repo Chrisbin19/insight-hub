174571e9a58e5fe244769aefd17075387f70b244
Chrisbin Sibi <chrisbinsibi19@gmail.com>
2025-09-10 23:35:06 +0530

    Update the review page so that the review becomes visible

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import StarRating from './StarRating'
-const Reviews = (reviews) => {
+const Reviews = ({reviews}) => {
     if (!Array.isArray(reviews)) {
     return <p>No reviews available</p>;
   }
